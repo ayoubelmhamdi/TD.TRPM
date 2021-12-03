d2b4267bb5c743d29a63424b7b6a96b8882c119f
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-12-03 01:33:33 +0100

    update

--- src/ex1.c
+++ src/ex1.c
@@ -1,9 +1,20 @@
+
+/* EX 1: permutation de tois nombre */
+
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
-  int a,b,c;
-  printf("Donner les valeur de l'equation a^x+bx+c=0\n");
-  // test
-  scanf("%d%d%d",&a,&b,&b);
+int main () {
+  int a,b,c,aide;
+  printf("Donner les 3 valeur de a b c\n");
+  scanf("%d%d%d",&a,&b,&c);
+
+  printf("vous aver entre   a=%d\tb=%d\tc=%d\n",a,b,c);
+  // permutation
+  aide=b;
+  a=b;
+  b=c;
+  c=aide;
+
+  printf("les valeur devien a=%d\tb=%d\tc=%d\n",a,b,c);
   return 0;
 }
